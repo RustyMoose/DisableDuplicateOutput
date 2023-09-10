451098a2c0b34c15833810c47efcc4396c16300c
JJ <jexs@moose.gg>
2023-09-10 09:16:41 -0500

    Harmony patch

---
+++ DisableDuplicateOutput/DisableDuplicateOutput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Harmony;
+
+namespace Moose.HarmonyMods.DisableDuplicateOutput
+{
+    [HarmonyPatch(typeof(ServerConsole), nameof(ServerConsole.OnEnable))]
+    internal static class PreventDuplicateOutput
+    {
+        private static bool Prepare() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        private static bool Prefix()
+        {
+            Console.WriteLine("Disabling ServerConsole to prevent duplicate output.");
+            return false;
+        }
+    }
+}
